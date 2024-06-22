514609570818cdb0847add21432d4ddc7082b583
zetdou <zyziupoz@gmail.com>
2024-06-22 13:55:22 +0200

    Update vite.config.js
    Set correct base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-02-feedback/",
+  base: "/goit-react-hw-04-feedback/",
   build: {
     outDir: "./dist",
   },
